3bf99f40ffc05d34fb5deb3595ca77dff724cced
apptreeso <superwebdev19@gmail.com>
2023-02-28 12:26:16 +0900

    add ITLDNameWrapper interface

---
+++ contracts/interfaces/ITLDNameWrapper.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ~0.8.17;
+
+interface ITLDNameWrapper {
+    function getData(uint256 tokenId) external view returns (address owner, uint32 fuses, uint64 expiry);
+}
