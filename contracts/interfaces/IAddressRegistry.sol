4cad41cb49ab46ac3025854527a3da69e541b6d0
apptreeso <superwebdev19@gmail.com>
2023-03-06 11:30:02 +0900

    update IAddressResgietry interface with the DCManagment contract

--- contracts/interfaces/IAddressRegistry.sol
+++ contracts/interfaces/IAddressRegistry.sol
@@ -4,5 +4,7 @@ pragma solidity 0.8.17;
 interface IAddressRegistry {
     function dc() external view returns (address);
 
+    function dcManagement() external view returns (address);
+
     function post() external view returns (address);
 }
