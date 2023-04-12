1ceb32eaf692939df824f5a68fd64e7338e5c88f
apptreeso <superwebdev19@gmail.com>
2023-04-12 11:57:48 +0900

    remove old contracts

--- contracts/interfaces/ITLDNameWrapper.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-interface ITLDNameWrapper {
-    function getData(uint256 tokenId) external view returns (address owner, uint32 fuses, uint64 expiry);
-}
