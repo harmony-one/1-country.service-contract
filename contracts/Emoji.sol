ca4fe5fc4221f1d5f549243ea2164d718518afd4
apptreeso <superwebdev19@gmail.com>
2023-04-17 22:51:20 +0900

    add the functions to add/trasnfer the emoji reactions

--- contracts/Emoji.sol
+++ contracts/Emoji.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
-import ".interfaces/IDC.sol";
+import "./interfaces/IDC.sol";
 
 contract Emoji is OwnableUpgradeable, PausableUpgradeable {
     // Enum for the emoji reactions
@@ -14,22 +14,37 @@ contract Emoji is OwnableUpgradeable, PausableUpgradeable {
         ONE_HUNDRED_PERCENT
     }
 
+    struct EmojiInfo {
+        EmojiType emojiType;
+        address owner;
+    }
+
     /// @dev DC contract
     address public dc;
 
-    /// @dev Revenue account
-    address public revenueAccount;
+    /// @dev DC TokenId -> EmojiInfo list
+    mapping(bytes32 => EmojiInfo[]) public emojiReactions;
 
     /// @dev Emoji Type -> Price
     mapping(EmojiType => uint256) public emojiReactionPrices;
 
-    mapping(string => mapping(EmojiType => uint256)) public emojiReactionCounters;
+    /// @dev Revenue account
+    address public revenueAccount;
 
-    mapping(EmojiType => uint256) public totalEmojiReactionCounter;
-
-    mapping(string => uint256) public lastEmojiReactionTimestamp;
-
+    event EmojiReactionAdded(address indexed by, string indexed name, address indexed owner, EmojiType emojiType);
     event RevenueAccountChanged(address indexed from, address indexed to);
+
+    modifier onlyDCOwner(string memory _name) {
+        address dcOwner = IDC(dc).ownerOf(_name);
+        require(msg.sender == dcOwner, "VanityURL: only DC owner");
+        _;
+    }
+
+    modifier whenDomainNotExpired(string memory _name) {
+        uint256 domainExpireAt = IDC(dc).nameExpires(_name);
+        require(block.timestamp < domainExpireAt, "VanityURL: expired domain");
+        _;
+    }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -40,7 +55,7 @@ contract Emoji is OwnableUpgradeable, PausableUpgradeable {
         __Pausable_init();
         __Ownable_init();
 
-        require(_dc != address(0), "VanityURL: zero address");
+        require(_dc != address(0), "Emoji: zero address");
 
         dc = _dc;
         revenueAccount = _revenueAccount;
@@ -48,6 +63,7 @@ contract Emoji is OwnableUpgradeable, PausableUpgradeable {
 
     function setRevenueAccount(address _revenueAccount) public onlyOwner {
         emit RevenueAccountChanged(revenueAccount, _revenueAccount);
+
         revenueAccount = _revenueAccount;
     }
 
@@ -55,27 +71,36 @@ contract Emoji is OwnableUpgradeable, PausableUpgradeable {
         emojiReactionPrices[_emojiType] = _price;
     }
 
-    function addEmojiReaction(string memory _name, EmojiType _emojiType) external payable whenNotPaused {
-        require(msg.value == emojiReactionPrices[_emojiType], "Invalid payment");
+    function addEmojiReaction(string memory _name, EmojiType _emojiType) external payable whenNotPaused whenDomainNotExpired(_name) {
+        bytes32 tokenId = keccak256(bytes(_name));
+        address dcOwner = IDC(dc).ownerOf(_name);
 
-        // Check if the emoji reaction counter should be initialized
-        if (lastEmojiReactionTimestamp[_name] < IDC(dc).registerAt(_name)) {
-            _resetEmojiReactionCounters(_name);
-        }
+        require(msg.value == emojiReactionPrices[_emojiType], "Emoji: invalid payment");
 
-        ++emojiReactionCounters[_name][_emojiType];
-        ++totalEmojiReactionCounter[_emojiType];
+        EmojiInfo memory emojiInfo = EmojiInfo({emojiType: _emojiType, owner: dcOwner});
+        emojiReactions[tokenId].push(emojiInfo);
     }
 
-    function _resetEmojiReactionCounters(string memory _name) _internal {
-        delete emojiReactionCounters[_name][EmojiType.ONE_ABOVE];
-        delete emojiReactionCounters[_name][EmojiType.FIRST_PRIZE];
-        delete emojiReactionCounters[_name][EmojiType.ONE_HUNDRED_PERCENT];
+    function transferEmojiReactions(string memory _name, address _receiver) external whenNotPaused onlyDCOwner(_name) whenDomainNotExpired(_name) {
+        bytes32 tokenId = keccak256(bytes(_name));
+        address sender = msg.sender;
+
+        for (uint256 i = 0; i < emojiReactions[tokenId].length;) {
+            EmojiInfo storage emojiInfo = emojiReactions[tokenId][i];
+
+            if (emojiInfo.owner == sender) {
+                emojiInfo.owner = _receiver;
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
     }
 
     function withdraw() external {
-        require(msg.sender == owner() || msg.sender == revenueAccount, "D1DC: must be owner or revenue account");
+        require(msg.sender == owner() || msg.sender == revenueAccount, "Emoji: must be owner or revenue account");
         (bool success, ) = revenueAccount.call{value: address(this).balance}("");
-        require(success, "D1DC: failed to withdraw");
+        require(success, "Emoji: failed to withdraw");
     }
 }
