990772fae2b50d46a5b3db9247ec565c599e2bf4
apptreeso <superwebdev19@gmail.com>
2023-04-19 21:47:30 +0900

    hot fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,7 +39,7 @@ module.exports = {
             saveDeployments: false
         },
         testnet: {
-            url: process.env.TESTNET_URL || "",
+            url: process.env.TESTNET_URL,
             accounts: { mnemonic: process.env.TEST_MNEMONIC },
             chainId: 1666700000,
             live: true,
@@ -47,7 +47,7 @@ module.exports = {
             saveDeployments: true
         },
         mainnet: {
-            url: process.env.MAINNET_URL || "",
+            url: process.env.MAINNET_URL,
             accounts: { mnemonic: process.env.MNEMONIC },
             chainId: 1666600000,
             live: true,
@@ -56,7 +56,7 @@ module.exports = {
             gas: 10e+6
         },
         s1: {
-            url: process.env.S1_URL || "",
+            url: process.env.S1_URL,
             accounts: { mnemonic: process.env.MNEMONIC },
             chainId: 1666600001,
             live: true,
