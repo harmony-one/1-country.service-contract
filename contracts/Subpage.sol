4c6052aa4a9fb0a12513d586676baefb528b4ce4
apptreeso <superwebdev19@gmail.com>
2023-04-24 19:08:16 +0900

    add initial Subpage contract

---
+++ contracts/Subpage.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "./interfaces/IDC.sol";
+
+contract Subpage is OwnableUpgradeable, PausableUpgradeable {
+    struct SubpageInfo {
+        string url;
+        string nameSpace;
+        address owner;
+    }
+
+    /// @dev DC contract
+    address public dc;
+
+    /// @dev DC TokenId -> SubpageInfo list
+    mapping(bytes32 => SubpageInfo[]) public subpages;
+
+    /// @dev Revenue account
+    address public revenueAccount;
+
+    event NewSubpageAdded(
+        address by,
+        string indexed name,
+        string url,
+        string indexed nameSpace,
+        address indexed owner
+    );
+    event SubpageDeleted(
+        address by,
+        string indexed name,
+        string url,
+        string indexed nameSpace,
+        address indexed owner
+    );
+    event SubpageUpdated(
+        address by,
+        string indexed name,
+        string oldURL,
+        string indexed oldNameSpace,
+        string newURL,
+        uint256 indexed newNameSpace,
+        address owner
+    );
+    event RevenueAccountChanged(address indexed from, address indexed to);
+
+    modifier onlyDCOwner(string memory _name) {
+        address dcOwner = IDC(dc).ownerOf(_name);
+        require(msg.sender == dcOwner, "VanityURL: only DC owner");
+        _;
+    }
+
+    modifier whenDomainNotExpired(string memory _name) {
+        uint256 domainExpireAt = IDC(dc).nameExpires(_name);
+        require(block.timestamp < domainExpireAt, "VanityURL: expired domain");
+        _;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address _dc, address _revenueAccount) external initializer {
+        __Pausable_init();
+        __Ownable_init();
+
+        require(_dc != address(0), "VanityURL: zero address");
+
+        dc = _dc;
+        revenueAccount = _revenueAccount;
+    }
+
+    /// @notice Set the DC contract address
+    /// @param _dc DC contract address
+    function setDCAddress(address _dc) external onlyOwner {
+        dc = _dc;
+    }
+
+    /// @notice Set the revenue account
+    /// @param _revenueAccount revenue account address
+    function setRevenueAccount(address _revenueAccount) public onlyOwner {
+        emit RevenueAccountChanged(revenueAccount, _revenueAccount);
+
+        revenueAccount = _revenueAccount;
+    }
+
+    /// @notice Withdraw funds
+    /// @dev Only owner of the revenue account can withdraw funds
+    function withdraw() external {
+        require(msg.sender == owner() || msg.sender == revenueAccount, "VanityURL: must be owner or revenue account");
+        (bool success, ) = revenueAccount.call{value: address(this).balance}("");
+        require(success, "VanityURL: failed to withdraw");
+    }
+
+    /// @notice Pause the contract
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice Unpause the contract
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
