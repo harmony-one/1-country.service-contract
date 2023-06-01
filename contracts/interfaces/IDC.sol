0dabcee4da21db9962d199bc1ef8647206376756
apptreeso <superwebdev19@gmail.com>
2023-06-02 02:24:19 +0900

    add the functions for the domain registration on IDC interface

--- contracts/interfaces/IDC.sol
+++ contracts/interfaces/IDC.sol
@@ -2,6 +2,12 @@
 pragma solidity 0.8.17;
 
 interface IDC {
+    function makeCommitment(string memory name, address owner, bytes32 secret) external view returns (bytes32);
+
+    function commit(bytes32 commitment) external;
+
+    function register(string calldata name, address owner, bytes32 secret) external payable;
+
     function available(string memory) external view returns (bool);
 
     function duration() external view returns (uint256);
